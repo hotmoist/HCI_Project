b46e87febd2c01af90695f48d038494c49c11050
Gyeyoung, Jung <gye0203@gmail.com>
2022-06-09 16:47:49 +0900

    [FEAT] 환경에 의한 알람 시작 구현 (*unfinished*)

--- app/src/main/java/com/example/hci_project/LightSensor.java
+++ app/src/main/java/com/example/hci_project/LightSensor.java
@@ -5,19 +5,24 @@ import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.PersistableBundle;
 import android.util.Log;
 import android.widget.Toast;
 
 import androidx.annotation.Nullable;
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
+
+import java.time.LocalTime;
 
 public class LightSensor extends AlramTimer implements SensorEventListener {
 
     public SensorManager sensorManager;
     public Sensor lightSensor;
     public String light = "";
+    public int darknessCount = 0;
     public String TAG = "LightSensor";
 
     @Override
@@ -38,12 +43,29 @@ public class LightSensor extends AlramTimer implements SensorEventListener {
         super.onResume();
         Log.d(TAG,"Resume");
     }
+    @RequiresApi(api = Build.VERSION_CODES.O)
     @Override
     public void onSensorChanged(SensorEvent sensorEvent) {
         if( sensorEvent.sensor.getType() == Sensor.TYPE_LIGHT){
             light = String.valueOf(sensorEvent.values[0]);
+            if(Integer.parseInt(light) < 500){
+                // FIXME : 어돕고 휴대폰 할때 light의 변수 값에 대해 파악해서 숫자 변경
+                darknessCount += 1;
+            }else{
+                darknessCount = 0;
+            }
+
+            if(darknessCount > 50 && getCurrentHour() > 22){
+                // TODO : 알람 trigger을 위한 broadcast 진행
+                //      : 특정 시간 이상일때 작동하도록 설정 (현재 오후 10로 설정)
+            }
             Log.d(TAG,"ligth : "+ light);
         }
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    public int getCurrentHour(){
+        return LocalTime.now().getHour();
     }
 
     @Override
